4beac71833c77deb86bf6879a99c2709a9bebc4e
ahmadshaikh008899 <onlineho40@gmail.com>
2024-12-19 14:17:20 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7748376075:AAFR7Rnsace57B08Of1ZFpNsg-cF3kP_c7Y")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RAHUL_DDOS_B")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1661744209")
+
+
+
+
+  
